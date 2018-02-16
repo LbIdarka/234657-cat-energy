4d2a0aea03e4127a69cd6d3d58aca61b330acb00
Alena Nikolaenko <lbidarka@gmail.com>
2018-02-16 23:19:27 +0300

    добавляет скрипт для карты на все страницы, правит id

--- source/js/script.js
+++ source/js/script.js
@@ -22,7 +22,7 @@ function initialize() {
     zoom: 17,
     center: new google.maps.LatLng(((screen.width < 1300) ? 59.93877121663107 : 59.939075), ((screen.width < 1300) ? 30.323274149999975 : 30.31965))
   };
-  var map = new google.maps.Map(document.getElementById("interactive-map"), mapOptions);
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
   var image = (screen.width < 768) ? "img/map-pin-small.png" : "img/map-pin.png";
   var myLatLng = new google.maps.LatLng(59.93877121663107, 30.323274149999975);
   var beachMarker = new google.maps.Marker({
